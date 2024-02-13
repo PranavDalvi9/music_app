56284dfcc56e6a5c05e300cf3d2ba96411c33dbc
PranavDalvi9 <pranavdalvi9@gmail.com>
2024-02-13 14:23:53 +0530

    initial

---
+++ lib/screen/Individual_screen/Individual_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndividualScreen extends StatefulWidget {
+  const IndividualScreen({super.key});
+
+  @override
+  State<IndividualScreen> createState() => _IndividualScreenState();
+}
+
+class _IndividualScreenState extends State<IndividualScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screen/splash_screen/splash_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/splash.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        width: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "Feel the beat",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 16),
+            SizedBox(
+              width: 200,
+              child: Text(
+                "Emmerse yourself into the world of music today",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFA5A5A5)),
+              ),
+            ),
+            SizedBox(height: 40),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 66),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(32),
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF842ED8),
+                    Color(0xFFDB28A9),
+                    Color(0XFF9D1DCA),
+                  ],
+                ),
+              ),
+              child: Text(
+                "Continue",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 88),
+          ],
+        ),
+      ),
+    );
+  }
+}
